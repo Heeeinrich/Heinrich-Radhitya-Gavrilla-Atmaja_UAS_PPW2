53148bbfed9533e6de56ec656990dd51760c7f9b
Heeeinrich <heinrichradhityagavr@mail.ugm.ac.id>
2024-12-03 14:55:54 +0700

    bugfixing transaksi.index

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -12,7 +12,7 @@
                     {{ Session::get('pesan') }}
                 </div>
             @endif
-            <table class="table table-bordered datatable">
+            <table class="table table-bordered datatable" id="transaksiTable">
                 <thead>
                 <tr>
                     <th>No</th>
@@ -24,25 +24,26 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach ( as $index => $data)
+                @foreach ($transaksi as $index => $data)
                     <tr>
                         <td>{{ $index+1 }}</td>
-                        <td>{{ \Carbon\Carbon::parse($data->tanggal_)->format('d/m/Y') }}</td>
-                        <td class="text-end">{{ number_format($data->, 0, '.', '.') }}</td>
-                        <td class="text-end">{{ number_format($data->, 0, '.', '.') }}</td>
-                        <td class="text-end">{{ number_format($data->, 0, '.', '.') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($data->tanggal_pembelian)->format('d/m/Y') }}</td>
+                        <td class="text-end">{{ number_format($data->total_harga, 0, '.', '.') }}</td>
+                        <td class="text-end">{{ number_format($data->bayar, 0, '.', '.') }}</td>
+                        <td class="text-end">{{ number_format($data->kembalian, 0, '.', '.') }}</td>
                         <td>
                             <div class="d-flex gap-2">
-                                <a href="{{ route('transaksi.edit', $data -> id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                <form action="{{ route('transaksi.destroy', $data -> id) }}" method="POST">
+                                <a href="{{ route('transaksi.edit', $data->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('transaksi.destroy', $data->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <button onclick="return confirm('Yakin mau dihapus?')" type="submit" class="btn btn-danger btn-sm">Hapus</button>
                                 </form>
-                                <a href="{{ route('transaksidetail.detail', $data -> id) }}" class="btn btn-info btn-sm">Detail</a>
+                                <a href="{{ route('transaksidetail.detail', $data->id) }}" class="btn btn-info btn-sm">Detail</a>
                             </div>
                         </td>
                     </tr>
+                @endforeach
                 </tbody>
             </table>
         </div>
