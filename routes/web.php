893d9400fafbd5ecc21292524ac686b7b71422e6
Heeeinrich <heinrichradhityagavr@mail.ugm.ac.id>
2024-12-03 15:11:44 +0700

    bugfixing route

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
@@ -27,11 +25,10 @@ Route::controller(LoginController::class)->group(function() {
     Route::post('/logout', 'logout')->name('logout');
 });
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 
-    Route::prefix('/transaksi')->group(function () {
+    Route::prefix('transaksi')->group(function () {
         Route::get('/', [TransaksiController::class, 'index'])->name('transaksi.index');
         Route::get('create', [TransaksiController::class, 'create'])->name('transaksi.create');
         Route::post('store', [TransaksiController::class, 'store'])->name('transaksi.store');
@@ -40,9 +37,9 @@ Route::middleware('auth')->group(function () {
         Route::delete('delete/{id}', [TransaksiController::class, 'destroy'])->name('transaksi.destroy');
     });
 
-    Route::prefix('/transaksidetail')->group(function ()  {
+    Route::prefix('transaksidetail')->group(function ()  {
         Route::get('/', [TransaksiDetailController::class, 'index'])->name('transaksidetail.index');
-        Route::get('/{id_transaksi}', [TransaksiDetailController::class, 'detail'])->name('transaksidetail.detail');
+        Route::get('{id_transaksi}', [TransaksiDetailController::class, 'detail'])->name('transaksidetail.detail');
         Route::get('edit/{id}', [TransaksiDetailController::class, 'edit'])->name('transaksidetail.edit');
         Route::put('update/{id}', [TransaksiDetailController::class, 'update'])->name('transaksidetail.update');
         Route::delete('delete/{id}', [TransaksiDetailController::class, 'destroy'])->name('transaksidetail.destroy');
